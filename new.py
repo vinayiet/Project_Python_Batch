5a989b627183742243716419dacd8bf1a66e0376
Vinay <vinayiet435@gmail.com>
2026-06-15 12:06:23 +0530

    something new has been added

--- new.py
+++ new.py
@@ -1,1 +1,12 @@
 print('I have new added new file')
+
+# echo "# Project_Python_Batch" >> README.md -> Here we are creating a new file 
+# git init -> Making simple folder to local repository (that can track all the changes)
+# git add README.md -> After making some change into that file just add that file to track
+
+# git commit -m "first commit" -> Just save the file using commit by some message
+# git branch -M main -> make main branch 
+# git remote add origin https://github.com/vinayiet/Project_Python_Batch.git -> 
+# this is the location where you will push your local code or local repository on the github
+
+# git push -u origin main -> this command is used to push all the commits 
